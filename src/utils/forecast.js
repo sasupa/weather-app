f9a62076dbe5c64e082672e3583ffe4e83eb3888
Sasu Paakkunainen <sasupa@gmail.com>
2019-09-17 17:50:56 +0300

    Added Windspeed to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (response.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain. You can expect a windspeed of ' + response.body.daily.data[0].windSpeed + " m/s.")
         }
     })
 }
