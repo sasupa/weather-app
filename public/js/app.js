465ece8fca721b2a1ab230ea61bd51b979597b96
Sasu Paakkunainen <sasupa@gmail.com>
2019-09-17 14:03:24 +0300

    Initial Commit

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+console.log("This should print in console, when loaded – on every page!")
+
+const weatherForm = document.querySelector("form")
+const search = document.querySelector("input")
+const msg1 = document.querySelector("#msg1")
+const msg2 = document.querySelector("#msg2")
+
+
+
+
+weatherForm.addEventListener("submit", (e) => {
+    msg2.textContent = ""
+    e.preventDefault()
+
+    const location = search.value
+    msg1.textContent = "Looking for weather in this location..."
+
+    fetch("http://localhost:3000/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            msg1.textContent = data.error
+        } else {
+            msg1.textContent = data.location
+            msg2.textContent = data.forecast
+        }
+    })
+})
+})
